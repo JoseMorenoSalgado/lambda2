bfd60e9a42cb83f5bcf3f2654e44645e87e902b2
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-05-21 10:06:25 -0600

    Bump Lambda2 for strict Moodle 5.2 compatibility

--- version.php
+++ version.php
@@ -5,29 +5,22 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Version information for Lambda2.
  *
  * @package   theme_lambda2
  * @copyright 2025 redPIthemes
- *
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026052150;
-$plugin->requires  = 2025041400;
+$plugin->version   = 2026052200;
+$plugin->requires  = 2026042000;
 $plugin->component = 'theme_lambda2';
-$plugin->release = '2.4.08-moodle51-52-replace';
-$plugin->maturity = MATURITY_STABLE;
+$plugin->release   = '2.5.00-moodle52';
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [
-    'theme_boost' => 2022041900
+    'theme_boost' => 2026042000,
 ];
